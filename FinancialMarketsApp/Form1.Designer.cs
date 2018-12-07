f2de0338054e167079063cd3c46600dbb34125da
kaajoj <kaajooj@gmail.com>
2018-12-07 02:25:06 +0100

    Added "log in" function

--- FinancialMarketsApp/Form1.Designer.cs
+++ FinancialMarketsApp/Form1.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passTextBox = new System.Windows.Forms.TextBox();
             this.cancelButton = new System.Windows.Forms.Button();
             this.loginButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -37,19 +37,22 @@
             this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // loginTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(139, 96);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(167, 20);
-            this.textBox1.TabIndex = 0;
+            this.loginTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.loginTextBox.Location = new System.Drawing.Point(139, 96);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(167, 23);
+            this.loginTextBox.TabIndex = 0;
             // 
-            // textBox2
+            // passTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(139, 135);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(167, 20);
-            this.textBox2.TabIndex = 1;
+            this.passTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.passTextBox.Location = new System.Drawing.Point(139, 135);
+            this.passTextBox.Name = "passTextBox";
+            this.passTextBox.PasswordChar = '*';
+            this.passTextBox.Size = new System.Drawing.Size(167, 23);
+            this.passTextBox.TabIndex = 1;
             // 
             // cancelButton
             // 
@@ -114,8 +117,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.loginButton);
             this.Controls.Add(this.cancelButton);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.passTextBox);
+            this.Controls.Add(this.loginTextBox);
             this.Name = "Form1";
             this.Text = "Financial Markets App";
             this.ResumeLayout(false);
@@ -125,8 +128,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passTextBox;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button loginButton;
         private System.Windows.Forms.Label label1;
--- FinancialMarketsApp/Form1.cs
+++ FinancialMarketsApp/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,9 +26,37 @@ namespace FinancialMarketsApp
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            Main main = new Main();
-            main.Show();
+            int count = 0;
+
+            string connectionString = @"Data Source = (localdb)\LocalDBKN; Initial Catalog = FinMarketsAppDB; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            String query = @"SELECT Count(*) FROM Users WHERE login = '" + loginTextBox.Text + "' AND password = '" + passTextBox.Text + "'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            DataTable dt = new DataTable();
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                count = Convert.ToInt32(reader[0].ToString());
+            }
+            MessageBox.Show(count.ToString());
+            connection.Close();
+
+            if (count == 1)
+            {
+                this.Hide();
+                Main main = new Main();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Wrong login or password, try again.");
+            }
+
+            
         }
     }
 }
